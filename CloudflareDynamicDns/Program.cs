4fbba7446a9f976b605fbabddc10c3bdf3ff9033
Bruno Garcia <bruno@brunogarcia.com>
2021-04-03 22:24:24 -0400

    more instrumentation`

--- CloudflareDynamicDns/Program.cs
+++ CloudflareDynamicDns/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using Sentry;
 using static System.Console;
@@ -10,126 +13,230 @@ using var _ = SentrySdk.Init(o =>
 {
     o.Dsn = "https://963b1a54ba3a48a69378145586f70b65@o117736.ingest.sentry.io/5703176";
     o.AttachStacktrace = true; // Event CaptureMessage includes a stacktrace
+    o.SendDefaultPii = true;
     o.TracesSampleRate = 1.0; // Capture transactions and spans
     o.Debug = true;
 });
-var transaction = SentrySdk.StartTransaction("dynamic-dsn", "dsn-update", "update dsn with public facing ip");
-var transactionStatus = SpanStatus.Ok; 
-// TODO: Expecting that now everything takes the trace-id 
-// var runId = Guid.NewGuid().ToString();
-// SentrySdk.ConfigureScope(s => s.SetTag("run-id", runId)); // All events sent will be correlated
+var transaction = SentrySdk.StartTransaction("dynamic-dns", "dns-update");
+SentrySdk.ConfigureScope(s => s.Transaction = transaction);
 
+var ctrlC = new CancellationTokenSource();
+Console.CancelKeyPress += (sender, args) =>
+{
+    args.Cancel = true;
+    Console.WriteLine("CTRL+C");
+    ctrlC.Cancel(true);
+};
+
+var transactionStatus = SpanStatus.Ok; 
+
+var serviceUrls = new []
+{
+    "https://checkip.amazonaws.com",
+    "https://api.ipify.org",
+    "https://api.my-ip.io/ip",
+};
+
+var updateDnsSpan = transaction.StartChild("update.dns");
 try
 {
-    var client = new HttpClient();
+    using var client = new HttpClient();
+    // spans from this integration end up picking up the wrong parent
+    // using var client = new HttpClient(new SentryHttpMessageHandler());
 
-    var amazonSpan = transaction.StartChild("task.start", "starting amazon aws ip check");
-    var publicIpAmazonTask = client.GetStringAsync("https://checkip.amazsonaws.com").CompleteTask(amazonSpan);
-
-    var ipifySpan = transaction.StartChild("task.start", "starting ipify ip check");
-    var publicIpIpifyTask = client.GetStringAsync("https://api.ipify.org").CompleteTask(ipifySpan);
-
-    var myipSpan = transaction.StartChild("task.start", "starting my-ip.io ip check");
-    var publicIpMyIpTask = client.GetStringAsync("https://api.my-ip.io/ip").CompleteTask(myipSpan);
-
-    var allTasks = new[] {publicIpAmazonTask, publicIpIpifyTask, publicIpMyIpTask};
-
-    while (allTasks.Length != 0)
+    var ipAddress = await GetIpAddress(client, updateDnsSpan, serviceUrls, ctrlC.Token);
+    transaction.SetTag("ip_address", ipAddress);
+    var processIpSpan = transaction.StartChild("process.ip");
+    try
     {
-        var firstTask = transaction.StartChild("task.any", "awaiting for the quickest task");
-        var task = Task.WaitAny(allTasks);
-        SentrySdk.AddBreadcrumb($"Resolved task at the index {task}");
-        firstTask.Finish();
+        WriteLine(ipAddress);
 
-        var completedTask = allTasks[task];
-        SentrySdk.ConfigureScope(s => s.SetTag("provider", ProviderName(completedTask)));
-
-        try
+        var successEvent = new SentryEvent
         {
-            var value = await completedTask; // Task should be completed (see Await above) but possibly throws here.
-            SentrySdk.AddBreadcrumb($"Resolved IP address {value}");
-            WriteLine(value);
-            SentrySdk.WithScope(s =>
-            {
-                s.SetTag("resolved-ip-address", value);
-                // Sentry Alert will be set to trigger if the following event isn't coming through at the expected rate.
-                // Example: every hour or so, depending how the cron job running this process is setup.
-                SentrySdk.WithScope(s =>
-                {
-                    // Stacktrace is always included, and even if we change the code, lets group everything under this key:
-                    s.SetFingerprint("success");
-                    SentrySdk.CaptureMessage("Successfully resolved public IP address.");
-                });
-            });
-            break;
-        }
-        catch (Exception e)
-        {
-            allTasks = allTasks.Where(t => !ReferenceEquals(completedTask, t)).ToArray();
-            if (allTasks.Length == 0)
-            {
-                SentrySdk.WithScope(s =>
-                {
-                    s.Level = SentryLevel.Fatal;
-                    // s.Extra[""]
-                    SentrySdk.CaptureException(e);
-                });
-                throw;
-            }
+            // Stacktrace is always included, and even if we change the code, lets group everything under this key:
+            Fingerprint = new[] {"success"},
+            Level = SentryLevel.Info,
+            Message = new SentryMessage {Formatted = $"Successfully resolved public IP address",},
+        };
+        successEvent.SetTag("resolved-ip-address", ipAddress);
 
-            SentrySdk.WithScope(s =>
-            {
-                s.Level = SentryLevel.Warning;
-                SentrySdk.CaptureException(e);
-            });
-            // Useful on the next event, if we have one:
-            SentrySdk.AddBreadcrumb($"Failed to get IP address.",
-                data: new Dictionary<string, string>
-                {
-                    {"exception", e.ToString()}
-                });
-        }
+        // Sentry Alert will be set to trigger if the following event isn't coming through at the expected rate.
+        // Example: every hour or so, depending how the cron job running this process is setup.
+        SentrySdk.CaptureEvent(successEvent);
     }
-
-    string ProviderName(Task taskInstance)
+    finally
     {
-        if (taskInstance == publicIpAmazonTask) return "amazon";
-        if (taskInstance == publicIpIpifyTask) return "ipify";
-        return taskInstance == publicIpMyIpTask ? "my-ip" : "unknown";
+        processIpSpan.Finish();
     }
+}
+catch (AllServicesFailedException)
+{
+    var failure = new SentryEvent
+    {
+        Level = SentryLevel.Fatal,
+        Message = new SentryMessage{Formatted = $"All {serviceUrls.Length} failed to resolve"},
+        Fingerprint = new[] {"all-services-failed"}
+    };
+    SentrySdk.CaptureEvent(failure);
 }
 catch (Exception e)
 {
-    SentrySdk.WithScope(s =>
+    SentrySdk.CaptureEvent(new SentryEvent(e)
     {
-        s.Level = SentryLevel.Fatal;
-        SentrySdk.CaptureException(e);
+        Level = SentryLevel.Fatal
     });
 
-    transactionStatus = SpanStatus.InternalError; // Make sure transaction fails
+    transactionStatus = SpanStatus.InternalError; // Make sure the transaction fails
+    // https://github.com/getsentry/sentry-dotnet/issues/919
+    // transaction.Status = SpanStatus.InternalError;
     throw;
 }
 finally
 {
+    ctrlC.Dispose();
+    updateDnsSpan.Finish();
     transaction.Finish(transactionStatus);
     // Wait for events to be sent.
-    WriteLine("Flush Transaction");
-    SentrySdk.FlushAsync(TimeSpan.FromSeconds(5));
+    await SentrySdk.FlushAsync(TimeSpan.FromSeconds(15));
 }
 
-static class TaskExtension
+static async Task<string> GetIpAddress(
+    HttpClient client, 
+    ISpan span,
+    IReadOnlyCollection<string> serviceUrls,
+    CancellationToken token)
 {
-    public static Task<TResult> CompleteTask<TResult>(this Task<TResult> task, ISpan span) =>
-        task.ContinueWith(t =>
+    var resolveSpan = span.StartChild("get.ip", "Resolve public IP address");
+    var cancelSource = CancellationTokenSource.CreateLinkedTokenSource(token);
+    cancelSource.CancelAfter(15000);
+    try
+    {
+        var queries = serviceUrls.AsParallel().Select(url =>
         {
-            span.Finish(t.ToSpanStatus());
-            return t.GetAwaiter().GetResult(); // Unwrap Task
-        });
+            var httpSpan = resolveSpan.StartChild("http.client", $"GET {url}");
+            return client.GetStringAsync(url, cancelSource.Token).ContinueWith(s =>
+            {
+                // TODO: Make status code mapping public
+                httpSpan.Finish(s.ToSpanStatus());
+                // Unwrap Result (task has completed):
+                return s.Result;
+            });
+        }).ToArray();
+
+        var taskToService = serviceUrls
+            .Zip(queries, (url, task) => (url, task))
+            .ToDictionary(k => k.task, v => v.url);
+        
+        while (queries.Length > 0)
+        {
+            cancelSource.Token.ThrowIfCancellationRequested();
+
+            var querySpan = resolveSpan.StartChild("query.loop");
+            var querySpanStatus = SpanStatus.Ok;
+
+            // Should roughly match the timing of the first HTTP request to complete
+            var taskSpan = querySpan.StartChild("task.any");
+            var completedTask = await Task.WhenAny(queries);
+            var status = completedTask.ToSpanStatus();
+            taskSpan.Finish(status);
+            var service = taskToService[completedTask];
+            querySpan.SetTag("service", service);
+
+            SentrySdk.AddBreadcrumb($"Completed request with '{service}'",
+                level: BreadcrumbLevel.Debug, data: new Dictionary<string, string> {{"status", status.ToString()}});
+            SentrySdk.ConfigureScope(s => s.SetTag("provider", service));
+
+            string? serviceResponse = null;
+            try
+            {
+                // Task should be completed (see 'await' above) but possibly throws here.
+                serviceResponse = await completedTask;
+                var parseSpan = querySpan.StartChild("parse.ip");
+                var parseSpanStatus = SpanStatus.Ok;
+                try
+                {
+                    serviceResponse = Regex.Replace(serviceResponse, "[^0-9.]", "");
+                    var ipAddress = IPAddress.Parse(serviceResponse).ToString();
+                    cancelSource.Cancel(); // Cancel any other get in-flight
+                    return ipAddress;
+                }
+                catch
+                {
+                    parseSpanStatus = SpanStatus.UnknownError;
+                    throw;
+                }
+                finally
+                {
+                    parseSpan.Finish(parseSpanStatus);
+                }
+            }
+            catch (Exception e)
+            {
+                var errorSpan = querySpan.StartChild("process.error");
+                try
+                {
+                    var evt = new SentryEvent(e);
+                    if (serviceResponse is not null)
+                    {
+                        evt.SetExtra("response", serviceResponse!);
+                    }
+
+                    evt.Message = new SentryMessage();
+
+                    queries = queries.Where(t => !ReferenceEquals(completedTask, t)).ToArray();
+                    if (queries.Length == 0)
+                    {
+                        querySpanStatus = SpanStatus.UnknownError;
+                        break;
+                    }
+
+                    evt.Level = SentryLevel.Warning;
+                    evt.Message.Formatted = $"Failed to resolve IP with {service}. Services left to try: {queries.Length}.";
+                    evt.Fingerprint = new[] {"one-of-n-failed"};
+                    SentrySdk.CaptureEvent(evt);
+
+                    // Useful on the next event, if we have one:
+                    SentrySdk.AddBreadcrumb($"Failed to get IP address.",
+                        data: new Dictionary<string, string>
+                        {
+                            {"ex.message", e.Message},
+                            {"ex.trace", e.StackTrace ?? "no available"}
+                        },
+                        level: BreadcrumbLevel.Warning);
+                }
+                finally
+                {
+                    errorSpan.Finish();
+                }
+            }
+            finally
+            {
+                querySpan.Finish(querySpanStatus);
+            }
+        }
+        throw new AllServicesFailedException();
+    }
+    finally
+    {
+        cancelSource.Dispose();
+        resolveSpan.Finish();
+    }
 }
+
 static class SpanExtension
 {
     public static SpanStatus ToSpanStatus(this Task task)
-        => task.Status == TaskStatus.RanToCompletion
-        // TODO: Assumes called form Completetask: Could consider cancelled, still running or other:
-            ? SpanStatus.Ok : SpanStatus.UnknownError;
+        => task.Status switch
+        {
+            TaskStatus.RanToCompletion => SpanStatus.Ok,
+            TaskStatus.Canceled => SpanStatus.Cancelled,
+            TaskStatus.Faulted => SpanStatus.InternalError,
+            _ => (SpanStatus)(-1) // No 'unknown' ?
+            // _ => SpanStatus.UnknownError
+        };
+}
+
+internal class AllServicesFailedException : Exception
+{
+    
 }
